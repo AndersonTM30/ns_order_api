211fcb2d7ece62a376fd3c88b59063c2f1501a44
AndersonTM30 <andersondev30@gmail.com>
2024-07-27 23:22:14 -0300

    test: add first role test

--- src/roles/roles.service.spec.ts
+++ src/roles/roles.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RolesService } from './roles.service';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { PrismaService } from '../prisma/prisma.module';
 
 describe('RolesService', () => {
   let service: RolesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService],
+      providers: [RolesService, PrismaService],
     }).compile();
 
     service = module.get<RolesService>(RolesService);
@@ -14,5 +16,16 @@ describe('RolesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be possible to create a new rule', async () => {
+    const roles: CreateRoleDto = {
+      slug: 'User',
+    };
+    jest
+      .spyOn(service, 'create')
+      .mockImplementation(async () => ({ slug: 'User' }));
+    const result = await service.create(roles);
+    expect(result).toEqual({ slug: 'User' });
   });
 });
