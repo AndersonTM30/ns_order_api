7e5f2f3dab4c9123b0664b7c5f607d60e1447e5a
AndersonTM30 <andersondev30@gmail.com>
2024-07-28 00:10:28 -0300

    test: add test of validation slug lenght

--- src/roles/roles.service.spec.ts
+++ src/roles/roles.service.spec.ts
@@ -2,13 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { PrismaService } from '../prisma/prisma.module';
+import { BadRequestException } from '@nestjs/common';
 
 describe('RolesService', () => {
   let service: RolesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService, PrismaService],
+      providers: [
+        RolesService,
+        {
+          provide: PrismaService,
+          useValue: {
+            role: {
+              findFirst: jest.fn().mockResolvedValue({
+                slug: 'User',
+              }),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<RolesService>(RolesService);
@@ -27,5 +40,28 @@ describe('RolesService', () => {
       .mockImplementation(async () => ({ slug: 'User' }));
     const result = await service.create(roles);
     expect(result).toEqual({ slug: 'User' });
+  });
+
+  it('should not be create an empty role', async () => {
+    const roles: CreateRoleDto = {
+      slug: '',
+    };
+    await expect(service.create(roles)).rejects.toThrow(BadRequestException);
+  });
+
+  it('should not create a role that already exists', async () => {
+    const roles: CreateRoleDto = {
+      slug: 'User',
+    };
+    await expect(service.create(roles)).rejects.toThrow('Role already exists');
+  });
+
+  it('should not be create a role if the slug is longer than 60 characters', async () => {
+    const roles: CreateRoleDto = {
+      slug: 'User gaçlkjaçlkjhadçlkjedlçkjyeriohyernbadlçjkhgadçlhkjapodihujaqploiyujadpioujaqpolijuh',
+    };
+    await expect(service.create(roles)).rejects.toThrow(
+      'The slug cannot be longer than 60 characters',
+    );
   });
 });
