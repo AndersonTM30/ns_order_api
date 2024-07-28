7f1d0c1f3089fcd63ed4011aaf39b60ed11c59da
AndersonTM30 <andersondev30@gmail.com>
2024-07-28 00:09:49 -0300

    feat: add validation slug length

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -15,6 +15,13 @@ export class RolesService {
     const roleExist = await this.prisma.role?.findFirst({
       where: { slug: data.slug },
     });
+
+    if (data.slug.length > 60) {
+      throw new BadRequestException(
+        'The slug cannot be longer than 60 characters',
+      );
+    }
+
     if (roleExist) {
       throw new BadRequestException('Role already exists');
     }
