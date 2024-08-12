83271077358b09436ce3d8c018260688899191f0
AndersonTM30 <andersondev30@gmail.com>
2024-08-11 22:26:05 -0300

    feat: adding the find all, find one and update permission data

--- src/permissions/dto/update-permission.dto.ts
+++ src/permissions/dto/update-permission.dto.ts
@@ -1,4 +1,17 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreatePermissionDto } from './create-permission.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdatePermissionDto extends PartialType(CreatePermissionDto) {}
+export class UpdatePermissionDto extends PartialType(CreatePermissionDto) {
+  id: number;
+  @ApiProperty({
+    description: 'The name of role',
+    example: 'Admin',
+    type: 'string',
+    maxLength: 60,
+  })
+  @IsString()
+  @IsNotEmpty()
+  slug: string;
+  updatedAt: Date;
+}
