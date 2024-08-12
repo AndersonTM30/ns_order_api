5ab4973fb713194d63c417ef2dfc35e7c1910da2
AndersonTM30 <andersondev30@gmail.com>
2024-08-11 22:24:36 -0300

    refactor: refactoring the prisma service

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,15 +1,9 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async enableShutdownHooks(app: INestApplication) {
-    (this.$on as any)('beforeExit', async () => {
-      await app.close();
-    });
-  }
-}
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
+export { PrismaService };
